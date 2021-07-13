33830d17f3af8b00bafb1a820047c8436b19a9b1
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2021-07-13 08:54:08 -0300

    feat(helper): add parseRepoFetch

---
+++ src/helpers/parseRepoFetch.ts
@@ -0,0 +1,14 @@
+export const parseRepoFetch = (repoFetch: any) => {
+  return {
+    userName: repoFetch.owner?.login,
+    userImg: repoFetch.owner?.avatar_url,
+    name: repoFetch.name,
+    description: repoFetch.description,
+    url: repoFetch.html_url,
+    contributions: repoFetch.network_count ?? 0,
+    usedBy: repoFetch.stargazers_count ?? 0,
+    stars: repoFetch.stargazers_count ?? 0,
+    forks: repoFetch.forks_count ?? 0,
+    languages_url: repoFetch.languages_url,
+  }
+}
