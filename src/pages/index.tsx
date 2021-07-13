4857e51accd1f38b9788d424bccd555bb3829af2
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2021-07-12 22:59:27 -0300

    fix(Home): add mock RepoCard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 
+import RepoCard from '../components/RepoCard/RepoCard'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,7 +12,19 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <RepoCard
+          userName='mauriciomutte'
+          userImg='https://github.com/mauriciomutte.png'
+          name='mauriciomutte.com'
+          description={`💻 My personal blog, I write about technology and things I'm learning. Built in GatsbyJS`}
+          url=''
+          contributions={4}
+          usedBy={0}
+          stars={0}
+          forks={2}
+        />
+      </main>
     </div>
   )
 }
