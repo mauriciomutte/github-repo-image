b272965465e0b99c95c8223aa6b2851403489960
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-06-04 17:10:27 -0300

    refactor: add user search in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
-import { GetServerSidePropsContext } from 'next'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
-import RepoCard, { RepoCardProps } from '../components/RepoCard'
-import { languagePerCent } from '../helpers/languagePerCent'
-import { parseRepoFetch } from '../helpers/parseRepoFetch'
 import styles from '../styles/Home.module.css'
 
-function Home(props: RepoCardProps) {
+function Home() {
+  const { push } = useRouter()
+  const [searchValue, setSearchValue] = useState('')
+
+  const handleSearchUser = () => {
+    push(`/user/${searchValue}`)
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,46 +21,15 @@ function Home(props: RepoCardProps) {
       </Head>
 
       <main className={styles.main}>
-        <RepoCard {...props} />
+        <h1 className={styles.title}>Search Github username</h1>
+        <input
+          value={searchValue}
+          onChange={e => setSearchValue(e.target.value)}
+        />
+        <button onClick={handleSearchUser}>Search</button>
       </main>
     </div>
   )
-}
-export async function getServerSideProps({ query }: GetServerSidePropsContext) {
-  const { username, repo } = query
-
-  if (!username || !repo) {
-    return { notFound: true }
-  }
-
-  const repoData = await fetch(
-    `https://api.github.com/repos/${username}/${repo}`
-  )
-    .then(response => {
-      if (response.status !== 200) {
-        throw new Error(`Failed to fetch repo data: ${response.statusText}`)
-      }
-
-      return response.json()
-    })
-    .then(data => parseRepoFetch(data))
-
-  const languageUrl = repoData.languages_url
-
-  if (!languageUrl) {
-    return { notFound: true }
-  }
-
-  const languages = await fetch(languageUrl)
-    .then(response => response.json())
-    .then(data => languagePerCent(data))
-
-  return {
-    props: {
-      ...repoData,
-      languages,
-    },
-  }
 }
 
 export default Home
