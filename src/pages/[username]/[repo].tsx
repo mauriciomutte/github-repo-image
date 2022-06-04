dc495c7f2203accc1d55fb3438ec905efc555b79
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-06-04 17:24:43 -0300

    feat: add repo page API github request

--- src/pages/[username]/[repo].tsx
+++ src/pages/[username]/[repo].tsx
@@ -1,31 +1,53 @@
-import { useRouter } from 'next/router'
+import { GetServerSidePropsContext } from 'next'
 
-import RepoCard from '../../components/RepoCard'
+import RepoCard, { RepoCardProps } from '../../components/RepoCard'
+import { languagePerCent } from '../../helpers/languagePerCent'
+import { parseRepoFetch } from '../../helpers/parseRepoFetch'
 import styles from '../../styles/Home.module.css'
 
-function UserRepoPage() {
-  const { query } = useRouter()
-
+function UserRepoPage(props: RepoCardProps) {
   return (
     <main className={styles.main}>
-      <h1>Repo: {query?.repo}</h1>
-      <RepoCard
-        userName='mauriciomutte'
-        userImg='https://github.com/mauriciomutte.png'
-        name='mauriciomutte.com'
-        description={`💻 My personal blog, I write about technology and things I'm learning. Built in GatsbyJS`}
-        url=''
-        contributions={4}
-        stars={0}
-        forks={2}
-        issues={2}
-        languages={[
-          { language: 'TypeScript', percentage: 90 },
-          { language: 'HTML', percentage: 10 },
-        ]}
-      />
+      <RepoCard {...props} />
     </main>
   )
+}
+
+export async function getServerSideProps({ query }: GetServerSidePropsContext) {
+  const { username, repo } = query
+
+  if (!username || !repo) {
+    return { notFound: true }
+  }
+
+  const repoData = await fetch(
+    `https://api.github.com/repos/${username}/${repo}`
+  )
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(`Failed to fetch repo data: ${response.statusText}`)
+      }
+
+      return response.json()
+    })
+    .then(data => parseRepoFetch(data))
+
+  const languageUrl = repoData.languages_url
+
+  if (!languageUrl) {
+    return { notFound: true }
+  }
+
+  const languages = await fetch(languageUrl)
+    .then(response => response.json())
+    .then(data => languagePerCent(data))
+
+  return {
+    props: {
+      ...repoData,
+      languages,
+    },
+  }
 }
 
 export default UserRepoPage
