606f7eaad02d1d1f8ef5c8504142115003c7492d
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-07-23 20:20:30 -0300

    feat: add Header in repo page

--- src/pages/[username]/[repo].tsx
+++ src/pages/[username]/[repo].tsx
@@ -1,5 +1,7 @@
 import { GetServerSidePropsContext } from 'next'
+import { useRouter } from 'next/router'
 
+import Header from '../../components/Header/Header'
 import RepoCard, { RepoCardProps } from '../../components/RepoCard'
 import { languagePerCent } from '../../helpers/languagePerCent'
 import { parseRepoFetch } from '../../helpers/parseRepoFetch'
@@ -7,10 +9,16 @@ import { getRepoDetails, getRepoLanguages } from '../../services/repos'
 import styles from '../../styles/Home.module.css'
 
 function UserRepoPage(props: RepoCardProps) {
+  const { userName, name } = props
+  const { back } = useRouter()
+
   return (
-    <main className={styles.main}>
-      <RepoCard {...props} />
-    </main>
+    <>
+      <Header title={`${userName}/${name}`} onBack={back} />
+      <main className={styles.main}>
+        <RepoCard {...props} />
+      </main>
+    </>
   )
 }
 
