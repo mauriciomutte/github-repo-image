5f8def78ff7fc4277b1caf6c54c82fce2bd7e042
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-07-24 00:13:24 -0300

    feat: add react query in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
+import { useState } from 'react'
+import {
+  DefaultOptions,
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const defaultOptions: DefaultOptions = {
+    queries: { useErrorBoundary: true, refetchOnWindowFocus: false },
+  }
+
+  const [queryClient] = useState(() => new QueryClient({ defaultOptions }))
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 export default MyApp
