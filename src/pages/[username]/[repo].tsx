831b985dec5e8b0fab742011dd19a400bdb84499
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-07-29 09:53:36 -0300

    feat: add useQuery in repo page

--- src/pages/[username]/[repo].tsx
+++ src/pages/[username]/[repo].tsx
@@ -1,39 +1,73 @@
 import { GetServerSidePropsContext } from 'next'
 import { useRouter } from 'next/router'
+import {
+  dehydrate,
+  QueryClient,
+  useQueries,
+  useQuery,
+} from '@tanstack/react-query'
 
 import Header from '../../components/Header/Header'
-import RepoCard, { RepoCardProps } from '../../components/RepoCard'
+import RepoCard from '../../components/RepoCard'
 import { languagePerCent } from '../../helpers/languagePerCent'
 import { parseRepoFetch } from '../../helpers/parseRepoFetch'
 import { getRepoDetails, getRepoLanguages } from '../../services/repos'
 import styles from '../../styles/Home.module.css'
 
-function UserRepoPage(props: RepoCardProps) {
-  const { userName, name } = props
-  const { back } = useRouter()
+function UserRepoPage() {
+  const { back, query } = useRouter()
+
+  const username = query.username as string
+  const repo = query.repo as string
+
+  const results = useQueries({
+    queries: [
+      {
+        queryKey: ['repoDetails'],
+        queryFn: () => getRepoDetails(username, repo),
+      },
+      {
+        queryKey: ['repoLanguages'],
+        queryFn: () => getRepoLanguages(username, repo),
+      },
+    ],
+  })
+
+  const isLoading = results.some(query => query.isLoading)
+  const [repoDetails, repoLanguages] = results
 
   return (
     <>
-      <Header title={`${userName}/${name}`} onBack={back} />
+      <Header title={`${username}/${repo}`} onBack={back} />
       <main className={styles.main}>
-        <RepoCard {...props} />
+        {isLoading ? (
+          <p>isLoading</p>
+        ) : (
+          <RepoCard
+            {...parseRepoFetch(repoDetails.data)}
+            languages={languagePerCent(repoLanguages.data)}
+          />
+        )}
       </main>
     </>
   )
 }
 
 export async function getServerSideProps({ query }: GetServerSidePropsContext) {
+  const queryClient = new QueryClient()
+
   const { username, repo } = query as { username: string; repo: string }
 
-  const [repoDetails, repoLanguages] = await Promise.all([
-    getRepoDetails(username, repo),
-    getRepoLanguages(username, repo),
-  ])
+  await queryClient.prefetchQuery(['repoDetails'], () =>
+    getRepoDetails(username, repo)
+  )
+  await queryClient.prefetchQuery(['repoLanguages'], () =>
+    getRepoDetails(username, repo)
+  )
 
   return {
     props: {
-      ...parseRepoFetch(repoDetails),
-      languages: languagePerCent(repoLanguages),
+      dehydratedState: dehydrate(queryClient),
     },
   }
 }
