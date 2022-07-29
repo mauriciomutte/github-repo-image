49fea55e169038bea39a974afc650b9088492f76
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-07-29 09:53:00 -0300

    feat: useQuery in user repo list page

--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -1,48 +1,51 @@
 import { GetServerSidePropsContext } from 'next'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query'
 
 import { getUserRepos, UserRepoResponse } from '../../services/user'
 import Header from '../../components/Header/Header'
 import styles from '../../styles/User.module.css'
 
-type UserPageProps = {
-  username: string
-  repos: UserRepoResponse[]
-}
+function UserPage() {
+  const { query, push } = useRouter()
 
-function UserPage({ username, repos }: UserPageProps) {
-  const { push } = useRouter()
+  const username = query.username as string
+
+  const { data: repos, isLoading } = useQuery(
+    ['repos', { username }],
+    () => getUserRepos(username),
+    { enabled: !!username }
+  )
 
   return (
     <>
       <Header onBack={() => push('/')} title={username} />
       <div className={styles.container}>
-        <ul>
-          {repos?.map(repo => (
-            <li>
-              <Link href={`${username}/${repo.name}`}>
-                <a>
-                  {repo.name} ({repo.stargazers_count})
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            {repos?.map(repo => (
+              <li key={repo.id}>
+                <Link href={`${username}/${repo.name}`}>
+                  <a>
+                    {repo.name} ({repo.stargazers_count})
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </>
   )
 }
 
-export async function getServerSideProps({ query }: GetServerSidePropsContext) {
-  const repos = await getUserRepos(query.username as string)
+export const getServerSideProps = ({ query }: GetServerSidePropsContext) => {
+  const username = query.username as string
 
-  return {
-    props: {
-      username: query.username,
-      repos,
-    },
-  }
+  return { props: { username } }
 }
 
 export default UserPage
